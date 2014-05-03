78f275cb835312a2525a1164e6c347e2fd187471
walkingway <walkingway@126.com>
2014-05-03 23:07:24 +0800

    Implement The Super Class Property

--- class.js
+++ class.js
@@ -16,6 +16,9 @@ function Class(param,parent) {
     	//首先查找对象自身是否有这个方法或者属性，如果没找到就会去对象的构造函数的原型对象中查找;因此这里要构造一个原型链。
     		constructor.prototype[key] = parent[key];
     }
+    
+    if (parent == undefined) parent = Object;
+    constructor.__super__ = parent;
 
     return constructor;
 }
