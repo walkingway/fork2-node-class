905073a7204af736a98deec26f3c34bc26466c24
walkingway <walkingway@126.com>
2014-05-03 22:54:02 +0800

    Revert "Implement Methods Inheritance"
    This reverts commit 91c9f34cc69b20a73965c66302d7224205d1ba2a.

--- class.js
+++ class.js
@@ -1,23 +1,23 @@
-function Class(param,parent) {
-    var constructor = param.initialize || function () {};
-    var key;
-    //子类的prototype对象，指向一个父类的实例，为了防止prototype对象的constructor被替换成父类的constructor必须替换回来
-    if (parent){
-    	constructor.prototype = new parent();
-    	constructor.prototype.constructor = constructor;
-    }
-    
-    for (key in param){
-    	constructor[key] = param[key];
-    }
+//constructor
 
-    for (key in parent){
-    	if(parent.hasOwnProperty(key) && typeof(parent[key]) == 'function' && key != 'initialize')
-    	//首先查找对象自身是否有这个方法或者属性，如果没找到就会去对象的构造函数的原型对象中查找;因此这里要构造一个原型链。
-    		constructor.prototype[key] = parent[key];
-    }
+function Class (c) {
+	if (c.initialize) {
+		this.constructor = c.initialize;
+	}  else {
+		this.constructor = function(){};
+	}
 
-    return constructor;
+	this.constructor.prototype = {
+		getA: function getA() {
+			return this.a;
+		},
+		getB: function getB() {
+			return this.b;
+		}
+	}
+
+	return this.constructor;
 }
 
 module.exports = Class;
+
