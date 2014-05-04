eef3bb9d617e51e351d50b21976388c11dda68b8
walkingway <walkingway@126.com>
2014-05-04 22:11:07 +0800

    rework on Implement Methods Inheritance && Implement Super

--- class.js
+++ class.js
@@ -1,26 +1,43 @@
 function Class(param,parent) {
-    var constructor = param.initialize || function () {};
+    var child = param.initialize || function () {};
     var key;
-    //子类的prototype对象，指向一个父类的实例，为了防止prototype对象的constructor被替换成父类的constructor必须替换回来
-    if (parent){
-    	constructor.prototype = new parent();
-    	constructor.prototype.constructor = constructor;
-    }
-    
-    for (key in param){
-    	constructor[key] = param[key];
-    }
 
     for (key in parent){
-    	if(parent.hasOwnProperty(key) && typeof(parent[key]) == 'function' && key != 'initialize')
-    	//首先查找对象自身是否有这个方法或者属性，如果没找到就会去对象的构造函数的原型对象中查找;因此这里要构造一个原型链。
-    		constructor.prototype[key] = parent[key];
+        if(parent.hasOwnProperty(key))child[key] = parent[key];
     }
+    parent = parent || Object;
+    // 临时构造函数ctor，用来打断父对象和子对象原型的直接链接，以防止父对象原型改变殃及子对象
+    function ctor(){};
+    //child.prototype copy parent.prototype，not ref；
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor();
+    //上面这两步会改变child.prototype.constructor为父类的构造函数，要重新把constructor指回自己
+    child.prototype.constructor = child;
     
-    if (parent == undefined) parent = Object;
-    constructor.__super__ = parent;
+    for (key in param){
+    	child.prototype[key] = param[key];
+    }
 
-    return constructor;
-}
+    child.__super__ = parent;
+    
+    //b.super --> B --> b.prototype --> new ctor --> a.prtootype
+    var _child = child;
+    child.prototype.super = function(){
+        //
+        if(_child.__super__.prototype[arguments[0]] !== undefined) 
+        {
+            //对于this.super("foo",a*10,b*100)要去掉第一个参数，然后再把后两个参数传递给父类相关的方法
+            var args = Array.prototype.slice.call(arguments,1);
+            var result = _child.__super__.prototype[arguments[0]].apply(this,args);
+            return result;
+        } 
+        else 
+        {
+            return undefined;
+        }
+    }
+   
+    return child;
+};
 
 module.exports = Class;
